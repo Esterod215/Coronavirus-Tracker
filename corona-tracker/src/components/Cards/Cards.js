bb4ecf0136ad962c90419d9131e386fd3ce9b6e9
Estevan <estevan.rodriguez10@yahoo.com>
2020-04-24 02:02:21 -0700

    displayed chart without proper styling. Need to style and add a country picker after

--- corona-tracker/src/components/Cards/Cards.js
+++ corona-tracker/src/components/Cards/Cards.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Cards.module.css";
 
 import CountUp from "react-countup";
+import cx from "classnames";
 
 //Mui imports
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
@@ -13,45 +14,81 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
   return (
     <div className={StylesContext.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          className={cx(styles.card, styles.infected)}
+          xs={12}
+          md={3}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
             <Typography variant="h5">
-              <CountUp start={0} end={confirmed ? confirmed.value : 0} />
+              <CountUp
+                start={0}
+                end={confirmed ? confirmed.value : 0}
+                separator=","
+              />
             </Typography>
-            <Typography color="textSecondary">Date</Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
             </Typography>
           </CardContent>
         </Grid>
 
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          className={cx(styles.card, styles.recovered)}
+          xs={12}
+          md={3}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Recovered
             </Typography>
             <Typography variant="h5">
-              <CountUp start={0} end={recovered ? recovered.value : 0} />
+              <CountUp
+                start={0}
+                end={recovered ? recovered.value : 0}
+                separator=","
+              />
             </Typography>
-            <Typography color="textSecondary">Date</Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               Number of recovered cases from COVID-19
             </Typography>
           </CardContent>
         </Grid>
 
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          className={cx(styles.card, styles.deaths)}
+          xs={12}
+          md={3}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Deaths
             </Typography>
             <Typography variant="h5">
-              <CountUp start={0} end={deaths ? deaths.value : 0} />
+              <CountUp
+                start={0}
+                end={deaths ? deaths.value : 0}
+                separator=","
+              />
             </Typography>
-            <Typography color="textSecondary">Date</Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               Number of deaths caused by COVID-19
             </Typography>
--- corona-tracker/src/components/Chart/Chart.js
+++ corona-tracker/src/components/Chart/Chart.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./Chart.module.css";
+
+import { Line, Bar } from "react-chartjs-2";
+import axios from "axios";
 
 const Chart = () => {
-  return <h1>Chart</h1>;
+  const [dailyData, setDailyData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://covid19.mathdro.id/api/daily")
+      .then(res => {
+        setDailyData(res.data);
+      })
+
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+  console.log(dailyData);
+
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: dailyData.map(data => {
+          return data.reportDate;
+        }),
+        datasets: [
+          {
+            data: dailyData.map(data => {
+              return data.confirmed.total;
+            }),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true
+          },
+          {
+            data: dailyData.map(data => data.deaths.total),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5",
+            fill: true
+          }
+        ]
+      }}
+    />
+  ) : null;
+  console.log(dailyData);
+
+  return lineChart;
 };
 
 export default Chart;
